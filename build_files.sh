1307f3f3fe618907f2563b55a5d5535b727be3b1
Phiazxo <prasopphol118@gmail.com>
2024-08-06 23:31:31 +0700

    commit

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,2 @@
-# build_files.sh
-pip3 install -r requirements.txt
-python3 manage.py collectstatic --noinput
+python3.12 -m pip install -r requirements.txt
+python3.12 manage.py collectstatic --noinput --clear
